7825482d4151b03ff9f5b4c2e38da1ba309d3da4
AdnanAjredini1 <adnanajredini144@gmail.com>
2024-11-05 09:03:49 +0100

    new database

--- src/main.jsx
+++ src/main.jsx
@@ -91,9 +91,9 @@ const router = createBrowserRouter([
 ]);
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+ 
     <Provider store={store}>
       <RouterProvider router={router} />
     </Provider>
-  </StrictMode>
+
 );
